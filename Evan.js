45b881e4d26cb28e3fce2aa5109ea7a2e97445e9
Koen Arnold <kostarnold@gmail.com>
2023-05-03 15:10:31 -0400

    ADded a test4 to Evan,js

--- Evan.js
+++ Evan.js
@@ -6,6 +6,10 @@ const Test2 = () => {
   console.log('This is a testThis is a testThis is a testThis is a test ');
 }
 
-const Test3 = () +> {
+const Test3 = () => {
+  console.log("this isn't a test this isn't a test this isn't a test")
+}
+
+const Test4 = () => {
   console.log("this isn't a test this isn't a test this isn't a test")
 }
